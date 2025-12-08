8c6dda29600c0c7ec0251475ac96d0d2e262a6f6
Burrito0211 <steven950211@gmail.com>
2025-12-09 00:18:04 +0800

    Add Mesh and Vertex classes for rendering utilities
    Introduced Mesh.h and Vertex.h to define mesh data structures and setup routines for rendering, including vertex attributes for position, normal, and texture coordinates. Also removed an unnecessary blank line in TrainView.H.

---
+++ src/RenderUtilities/Mesh.h
@@ -0,0 +1,56 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include "RenderUtilities/Shader.h"
+#include "RenderUtilities/Vertex.h"
+#include "RenderUtilities/Texture.h"
+#include <vector>
+
+class Mesh {
+    public:
+        // mesh data
+        std::vector<Vertex>       vertices;
+        std::vector<unsigned int> indices;
+        std::vector<Texture2D>      textures;
+
+        Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture2D> textures) {
+            this->vertices = vertices;
+            this->indices = indices;
+            this->textures = textures;
+
+            setupMesh();
+        }   
+
+        void Draw(Shader &shader);
+    private:
+        //  render data
+        unsigned int VAO, VBO, EBO;
+
+        void setupMesh() {
+            glGenVertexArrays(1, &VAO);
+            glGenBuffers(1, &VBO);
+            glGenBuffers(1, &EBO);
+        
+            glBindVertexArray(VAO);
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+            glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);  
+
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), 
+                        &indices[0], GL_STATIC_DRAW);
+
+            // vertex positions
+            glEnableVertexAttribArray(0);	
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+            // vertex normals
+            glEnableVertexAttribArray(1);	
+            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+            // vertex texture coords
+            glEnableVertexAttribArray(2);	
+            glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+
+            glBindVertexArray(0);
+        }  
+};  
+#endif
---
+++ src/RenderUtilities/Vertex.h
@@ -0,0 +1,12 @@
+#ifndef VERTEX_H
+#define VERTEX_H
+
+#include <glm/glm.hpp>
+
+struct Vertex {
+    glm::vec3 Position;
+    glm::vec3 Normal;
+    glm::vec2 TexCoords;
+};
+
+#endif // VERTEX_H
